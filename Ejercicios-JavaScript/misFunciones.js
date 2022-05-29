6cc1b4d92348e533e6d90d66bb778f21cb59054e
antonio <antomora346@gmail.com>
2022-05-28 23:16:10 -0300

    Actualizacion

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,11 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+function cambiarUnidades (id, valor){
+    if(isNaN(valor)) {
+        alert("Se ingreso un valor invalido");
+        document.lasUnidades.unid_metro.value ="";
+    }else if(id=="metro"){
+        document.lasUnidades.unid_pulgada.value = ""
+    }
+}
